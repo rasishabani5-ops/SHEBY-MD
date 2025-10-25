fc2a48e3b4ef5e910133c336bd3f4e86904b019a
rasishabani5-ops <rasishabani5@gmail.com>
2025-10-25 16:34:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'SHEBY Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "SHEBY Bot",
+  botOwner: '255684759407', // Your name
+  ownerNumber: '255684759407', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
